b3c01d060fd27dba894c3dfab59a0ae0aca887a2
Zachary Childers <zdchilders10@sccsc.edu>
2020-11-02 13:03:52 -0500

    Fixed with a loop, now it actually works like it should! huzzah.
    Will add additional food selections.

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import random
-
-
-def user_answer(param):
-    pass
-
-
-while user_answer != 0:
+while True:
     foodList = ["Steak",
                 "Lasagna",
                 "Meatloaf",
-                "Turkey", 
+                "Turkey",
                 "Shepherds Pie",
                 "Mcdonalds",
                 "Chickfila",
@@ -19,8 +13,8 @@ while user_answer != 0:
                 "Burger King",
                 "Pizza"]
     print("We're gonna eat: ", random.choice(foodList))
-    user_answer(input("Would you like to stop? Enter '0': "))
-    if input == '0'.format():
-        exit(0)
+    user_input = input("Would you like to stop? Enter 'end': ")
+    if user_input.lower() == "end":
+        break
     else:
         continue
