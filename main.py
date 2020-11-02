25e994e191c7fe047ce3dca160110fcf70a53f00
Zachary Childers <zdchilders10@sccsc.edu>
2020-11-02 14:03:19 -0500

    200,000 meals are at ready with a million more on the way.
    Still adding more to the list so that there are less repeats.

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import random
+
 while True:
-    foodList = ["Steak",
-                "Lasagna",
-                "Meatloaf",
-                "Turkey",
-                "Shepherds Pie",
-                "Mcdonalds",
-                "Chickfila",
-                "Wendy's",
-                "Arby's",
-                "Dairy Queen",
-                "Burger King",
-                "Pizza"]
-    print("We're gonna eat: ", random.choice(foodList))
-    user_input = input("Would you like to stop? Enter 'end': ")
+    food_list = ["Steak",
+                 "Lasagna",
+                 "Meatloaf",
+                 "Turkey",
+                 "Shepherds Pie",
+                 "McDonalds",
+                 "Chickfila",
+                 "Wendy's",
+                 "Arby's",
+                 "Dairy Queen",
+                 "Burger King",
+                 "Pizza",
+                 "Spaghetti",
+                 "Sloppy Joe Tacos w/ Tabasco",
+                 "Pork-Chops",
+                 "Seafood",
+                 "Burgers",
+                 "Tacos",
+                 "Chinese",
+                 "Kielbasa",
+                 "Crockpot Beef + Peppers",
+                 "Crack-chicken",
+                 "Crockpot BBQ Chicken",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 "",
+                 ""]
+    print("We're gonna eat: ", random.choice(food_list))
+    user_input = input("\nWould you like to stop?\nEnter 'end': ")
     if user_input.lower() == "end":
         break
     else:
